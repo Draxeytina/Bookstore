b627ff8ce4f0b7cd72fd16444c58e762d16f31e9
Tinashe Murambinda <tmurambinda01@gmail.com>
2022-07-13 23:17:09 +0200

    update Bookslist, merge with book, delete book

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,9 +1,26 @@
-import Book from './Book';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function BooksList() {
+export default function BooksList({ Author, Title, Index }) {
   return (
     <ul>
-      <Book />
+      <li className={Index}>
+        Title:
+        {' '}
+        {Title}
+        {' '}
+        by
+        {' '}
+        {Author}
+        {' '}
+        <button type="button">Delete</button>
+      </li>
     </ul>
   );
 }
+
+BooksList.propTypes = {
+  Author: PropTypes.string.isRequired,
+  Title: PropTypes.string.isRequired,
+  Index: PropTypes.number.isRequired,
+};

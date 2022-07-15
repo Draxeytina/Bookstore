a1dcb3b6ef3005b02562453f3ec1fdf7a3935d22
Tinashe Murambinda <tmurambinda01@gmail.com>
2022-07-15 17:42:55 +0200

    tree left side

---
+++ src/components/Book.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import 'react-circular-progressbar/dist/styles.css';
+import Remove from './DeleteBook';
+
+const Book = (props) => {
+  const { id, title, author } = props;
+
+  return (
+    <li className={id}>
+      Title:
+      {' '}
+      {title}
+      {' '}
+      by
+      {' '}
+      {author}
+      {' '}
+      <Remove />
+    </li>
+  );
+};
+
+Book.defaultProps = {
+  id: '',
+  title: '',
+  author: '',
+};
+
+Book.propTypes = {
+  id: PropTypes.string,
+  title: PropTypes.string,
+  author: PropTypes.string,
+};
+
+export default Book;
---
+++ src/components/DeleteBook.js
@@ -0,0 +1,18 @@
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/Books';
+
+const Remove = () => {
+  const dispatch = useDispatch();
+
+  const handleRemove = (event) => {
+    const { target } = event;
+    const id = target.parentNode.classList[0];
+    dispatch(removeBook(id));
+  };
+
+  return (
+    <button type="button" onClick={handleRemove}>Delete</button>
+  );
+};
+
+export default Remove;
